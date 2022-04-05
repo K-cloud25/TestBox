cba89010d6adc9477327954a99173b6cf62cc539
Deadheaven <rawoorkar30@gmail.com>
2022-04-05 21:27:19 +0530

    24push

--- app/src/main/java/org/textbox/testbox/Main_Assignment.kt
+++ app/src/main/java/org/textbox/testbox/Main_Assignment.kt
@@ -1,17 +1,33 @@
 package org.textbox.testbox
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import org.textbox.testbox.databinding.ActivityMainAssignmentBinding
 import org.w3c.dom.Text
+import java.net.URI
 
 class Main_Assignment : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainAssignmentBinding
+    private lateinit var fireAuth: FirebaseAuth
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var firebaseStorage: FirebaseStorage
+    lateinit var uri: URI
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +40,31 @@ class Main_Assignment : AppCompatActivity() {
         val branchPath = intent.getStringExtra("branchPath").toString()
         val branchName = intent.getStringExtra("branch").toString()
 
+
+
+
+        val database= FirebaseDatabase.getInstance ().getReference(  "Uploads pdf")
+        val storage = FirebaseStorage.getInstance().getReference("uploads pdf")
+
+
+
         binding.addBtn.setOnClickListener {
             AddPDF(binding.root.context,yearPath,branchPath,branchName)
+
+
         }
+
+
+
+    }
+
+
+    private fun SelectPDF() {
+         val intent = Intent()
+        intent.setType ("pdf/*")
+        intent.setAction (Intent.ACTION_GET_CONTENT)
+        val PDF : Int=0
+        startActivityForResult (Intent.createChooser(intent,  "Select PDF"), PDF)
     }
 
     private fun AddPDF(context : Context,yearPath : String,branchPath : String,branchName : String){
@@ -43,5 +81,14 @@ class Main_Assignment : AppCompatActivity() {
 
         val branchTV : TextView? = root.findViewById(R.id.branchPathTV)
         branchTV?.text = "Branch : $branchName"
+
+        val uploadBtn : Button? = root.findViewById(R.id.uploadPdfBtn)
+        if (uploadBtn != null) {
+            uploadBtn.setOnClickListener{
+                SelectPDF()
+            }
+        }
     }
+
+
 }
